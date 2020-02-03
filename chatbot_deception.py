136f75d4388294ee1d4fffba38b8eb5f94046a83
Rebecca Ramnauth <rebecca.ramnauth@yale.edu>
2020-02-03 08:57:05 -0500

    updated main paradigm file

--- chatbot_deception.py
+++ chatbot_deception.py
@@ -1,28 +1,32 @@
+# Rebecca Ramnauth
+# last update: 01 10 2020
+# chatbot deception study s'2020
+
 from setup import *
-import subprocess
+import subprocess, random
 
 from psychopy import visual, gui
 import sys
-from tools.easygui import *
-from tools.experiment import *
-from tools.experiment_dual_monitor import *
+from tools2.easygui import *
+from tools2.experiment import *
+from tools2.experiment_dual_monitor import *
 
-from tools.Checkerboard import *
+#from tools.Checkerboard import *
 
 isMusic=0
 msg ="Experiments"
 title = "Experiment List"
 choices0 = ["00: Rebecca's Chatbot Paradigm"]
 nC=len(choices0) 
-x=runrecord("eyewall",nC,360)
+x=runrecord("rebecca",nC,360)
 choices=[]
 isTest=0 
-subject='hh';
+sequence=25
 
 # generate menu GUI
 for i in range(nC):
     if x.check(i)==0:
-        choices.append("%s" % choices0[i])
+        choices.append(" %s" % choices0[i])
     else:
         choices.append("%s done" % choices0[i])
         
@@ -33,9 +37,9 @@ while 1:
     print i
     if i == 0:
         MaterialFolder='Rebecca_Chatbot'
-        paradigmdescript="Michelle_%s.csv" %(subject)
+        paradigmdescript="sequence_u_%s.csv" %(sequence)
         keyboardrecord="Chat.txt"
-        #subprocess.call('vlcscreenrecord.bat Michelle_%s 215' % (datetime.datetime.now().strftime("%Y-%m-%d%H_%M_%S")))
+        #subprocess.call('vlcscreenrecord.bat chatbot_%s 215' % (datetime.datetime.now().strftime("%Y%m%d_%H%M%S")))
         e=experiment_dual_monitor(instrument,Nirs,eeglist,2,MaterialFolder,paradigmdescript,keyboardrecord,longTrigList=[2,4])
         e.winlist=[1,2];
         e.keysimulator="3";
